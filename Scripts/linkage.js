105d29e4d101c1e264f40599ed326639b2bc0506
Bokang Lethlogonolo Maropola <148450740+BokangMaropola@users.noreply.github.com>
2024-08-28 13:48:55 +0200

    Add files via upload

---
+++ Scripts/linkage.js
@@ -0,0 +1,12 @@
+function openCalculator() {
+    window.open("Projects/Calculator/index.html", "_blank");
+  }
+  function openJohnnyHart() {
+    window.open("Projects/Johnny_Hart/index.html", "_blank");
+  }
+  function openSoloLearn() {
+    window.open("https://www.sololearn.com/en/profile/29597731", "_blank");
+  }
+  function openCodecademy() {
+    window.open("https://www.codecademy.com/profiles/BokangMaropola", "_blank");
+  }
